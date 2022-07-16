dbc51415cfa792dcdc84044966b9663bfc74fdf3
arturRzeszutko <80632516+arturRzeszutko@users.noreply.github.com>
2022-07-16 20:52:20 +0200

    Dodanie bazy danych i jej obsługa + utworzenie bazy danych Gdyby jej nie było

--- WpfApp/App.xaml.cs
+++ WpfApp/App.xaml.cs
@@ -1,17 +1,38 @@
-﻿using System;
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using WpfApp.Data;
 
 namespace WpfApp
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+
     public partial class App : Application
     {
+        private ServiceProvider serviceProvider;
+
+        public App()
+        {
+            ServiceCollection services = new ServiceCollection();
+            services.AddDbContext<ProductDbContext>(option =>
+            {
+                option.UseSqlite("Data Source = Product.db");
+            });
+
+            services.AddSingleton<MainWindow>();
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void OnStartup(object s, StartupEventArgs e)
+        {
+            var mainWindow = serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+        }
     }
 }
--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -12,17 +12,59 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp.Data;
 
 namespace WpfApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        ProductDbContext context;
+        Product NewProduct = new Product();
+        Product selectedProduct = new Product();
+
+
+        public MainWindow(ProductDbContext context)
         {
+            this.context = context;
             InitializeComponent();
+            GetProducts();
+            NewProductGrid.DataContext = NewProduct;
+        }
+
+
+        private void GetProducts()
+        {
+            ProductDG.ItemsSource = context.Products.ToList();
+        }
+
+        private void AddItem(object s, RoutedEventArgs e)
+        {
+            context.Products.Add(NewProduct);
+            context.SaveChanges();
+            GetProducts();
+            NewProduct = new Product();
+            NewProductGrid.DataContext = NewProduct;
+        }
+
+        private void UpdateItem(object s, RoutedEventArgs e)
+        {
+            context.Update(selectedProduct);
+            context.SaveChanges();
+            GetProducts();
+        }
+
+        private void SelectProductToEdit(object s, RoutedEventArgs e)
+        {
+            selectedProduct = (s as FrameworkElement).DataContext as Product;
+            UpdateProductGrid.DataContext = selectedProduct;
+        }
+
+        private void DeleteProduct(object s, RoutedEventArgs e)
+        {
+            var productToDelete = (s as FrameworkElement).DataContext as Product;
+            context.Products.Remove(productToDelete);
+            context.SaveChanges();
+            GetProducts();
         }
     }
 }
